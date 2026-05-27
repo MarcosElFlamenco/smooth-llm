5a50e0f4ae7eed7f98156c7cbbfbb332b3eebf15
Oscar Garnier <ogpourcommander@gmail.com>
2026-05-27 17:45:30 +0200

    printing output prompt

--- defenses/defenses.py
+++ defenses/defenses.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import gc
 from AutoDAN.utils.string_utils import autodan_SuffixManager
-from AutoDAN.utils.eval_utils import check_for_attack_success
+from AutoDAN.utils.eval_utils import generate
 #import lib.perturbations as perturbations
 
 
@@ -50,6 +50,42 @@ class NoDefense(Defense):
         super(NoDefense, self).__init__(target_model,tokenizer, conv_template)
 
     @torch.no_grad()
+    def __call__(self, goal, target, adv_suffix, gen_config, batch_size=64, max_new_len=64):
+        suffix_manager = autodan_SuffixManager(
+            tokenizer=self.tokenizer,
+            conv_template=self.conv_template,
+            instruction=goal,
+            target=target,
+            adv_string=adv_suffix,
+        )
+        gen_str = self.tokenizer.decode(
+            generate(
+                self.target_model,
+                self.tokenizer,
+                suffix_manager.get_input_ids(adv_string=adv_suffix).to(self.target_model.device),
+                suffix_manager._assistant_role_slice,
+                gen_config=gen_config,
+            )
+        ).strip()
+
+        gc.collect()
+        torch.cuda.empty_cache()
+        return gen_str
+
+class Perturb(Defense):
+    """
+    No defense
+    
+    """
+
+    def __init__(self, 
+        target_model,
+        tokenizer,
+        conv_template
+    ):
+        super(Perturb, self).__init__(target_model,tokenizer, conv_template)
+
+    @torch.no_grad()
     def __call__(self, goal, target, adv_suffix, batch_size=64, max_new_len=64):
         suffix_manager = autodan_SuffixManager(
             tokenizer=self.tokenizer,
@@ -69,7 +105,4 @@ class NoDefense(Defense):
         gc.collect()
         torch.cuda.empty_cache()
         return gen_str
-
-
-
 
